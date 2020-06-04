d9ae28f83e8e8a473234aea361786962239f453e
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2020-06-04 11:03:20 -0300

    Adding the history feature and the general img to the readme.md

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ function calculate() {
     var binary = document.querySelector("[name=binary]").value
     var decimal = document.querySelector("[name=decimal]")
 
-    
-
     var validate = []
     var validateStatus = false
 
@@ -21,9 +19,11 @@ function calculate() {
     }
 
     if (validateStatus === true) {
-        decimal.value = parseInt(binary, 2);
+        decimal.value = parseInt(binary, 2)
+        let decimalHistory = parseInt(binary, 2)
+        getHistory(binary, decimalHistory)
     }
-
+       
 }
 
 function printError() {
@@ -38,5 +38,18 @@ function cleanError() {
     }
 }
 
+function getHistory(binary, decimal) {
+    
+    let historyLength = document.querySelectorAll("[class=history]").length
+
+    if (historyLength < 4) { 
+        let history = document.querySelector("[class=container-history]")
+        history.innerHTML += `<p class="history">${binary} = ${decimal}</p><br>`
+    } else {
+        let history = document.querySelector("[class=container-history]")
+        history.innerHTML += `<p class="history">${binary} = ${decimal}</p><br>`
+    }
+
+}
 document.querySelector("[name=binary]")
 document.addEventListener("change", calculate)
